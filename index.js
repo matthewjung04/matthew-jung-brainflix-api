7f7f11a02f15551a49af979d6616f04d5f314329
matthewjung04 <matthew.jung95@gmail.com>
2024-09-18 21:15:17 -0400

    add get /videos route for getting list of videos

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express'
-import axios from 'axios'
 import fs from 'fs'
+import cors from 'cors'
+import { v4 as uuidv4 } from 'uuid'
+import 'dotenv/config'
 
 const app = express();
+const { PORT, ORIGIN } = process.env;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(cors({ PORT, ORIGIN }));
 
 /* /videos
 GET
@@ -12,13 +20,74 @@ POST
  - Adds a new video to the video list
  - A unique id is generated for each video added
 */
-app.route('/video')
-  .get(function(req,res) {
-    res.send('get list of videos');
+app.route('/videos')
+  .get((req,res) => {
+    const getVideoList = fs.readFileSync('./data/videos.json',{encoding: 'utf8'})
+    console.log(fs.readFileSync('./data/videos.json',{encoding: 'utf8'}))
+    res.send(getVideoList);
   })
-  .post(function(req,res) {
-    res.send('add a video');
-  })
+ 
+
+  // .post(function(req,res) {
+  
+  //   /* Simplified video data */
+  //   const newVideoId = uuidv4();
+  //   const title = req.body.title;
+  //   const channel = "BrainStation";
+  //   const image = req.body.image;
+  //   const description = req.body.description;
+  //   const views = 0;
+  //   const likes = 0;
+  //   const timestamp = Date.now();
+  //   const comments = [];
+
+  //   /* Partial video details */
+  //   const newVideo = {
+  //     id: newVideoId,
+  //     title: title,
+  //     channel: channel,
+  //     image: image,
+  //   }
+
+  //   /* Full video details */
+  //   const newFullVideo = {
+  //     id: newVideoId,
+  //     title: title,
+  //     channel: channel,
+  //     image: image,
+  //     description: description,
+  //     views: views,
+  //     likes: likes,
+  //     timestamp: timestamp,
+  //     comments: comments
+  //   }
+ 
+  //   /* Import json file containing simplified list of videos */
+  //   const getVideos= () => {fs.readFileSync('./data/videos.json',{encoding: 'utf8'})}
+  //   let readVideos = JSON.parse(getVideos());
+  //   /* update data array containing simplified video list */
+  //   readVideos.push(newVideo);
+  //   /* write the new updated array for simplified video list and callback confirmation */
+  //   fs.writeFile('./data/videos.json',
+  //     JSON.stringify(readVideos), () => {
+  //       res.json(`${title} has been uploaded`);
+  //     }
+  //   );
+
+  //   /* Import json file containing detailed list of videos */
+  //   const getFullVideos = () => { fs.readFileSync('./data/video-details.json',{encoding: 'utf8'})}
+  //   let readFullVideos = JSON.parse(getFullVideos());
+  //   /* update data array containing detailed video list */
+  //   readFullVideos.push(newFullVideo);
+  //   /* write the new updated array for detailed video list and callback confirmation */
+  //   fs.writeFile('./data/video-details.json',
+  //     JSON.stringify(readFullVideos), () => {
+  //       res.json(`${title} has been added to next video list`)
+  //     }
+  //   );
+
+  // })
+;
 
 /* /videos/:id
  - :id must be swapped out with the id of a video as found in the list of videos
@@ -37,13 +106,13 @@ app.route('video/:id')
   })
 
 /* /videos/:id/comments
-POST
+POST 
  - :id must be swapped out with the numeric id of a video as found in the list of videos
  - Creates a new comment for a specific video
 */
-app.post(function(req,res) {
-  res.send('add a comment');
-})
+app.post('/videos/:id/comments', function(req,res) {
+
+});
 
 /* /vidoes/:videoId/comments/:commentId
 DELETE
@@ -51,6 +120,9 @@ DELETE
  - :videoId must be swapped out with the numeric id of a video as found in the list of videos
  - :commentId must be swapped out with the numeric id of a comment as found in the list of comments for the given video
 */
-app.delete(function(req,res) {
-  res.send('delete a comment')
-})
+// app.delete(function(req,res) {
+//   res.send('delete a comment')
+// });
+
+/* Run the server */
+app.listen(PORT, () => {console.log(`Litsening on ${PORT}`)})
